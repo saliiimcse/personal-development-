c92cd53acb33cea42656072ee29140f89e77c878
Salim Ahmed <selim11bd@gmail.com>
2022-12-20 00:36:39 +0600

    first commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,26 +5,47 @@ import Book from '../Book/Book';
 
 const Layout = () => {
     const [book, setBook] = useState([]);
-
+    const [addCart, setCart] = useState(null);
+    console.log(addCart);
+    // const timeInteger = parseInt(addCart);
+    // const sum = parseInt(addCart).reduce((partialSum, a) => partialSum + a, 0)
+    // console.log(addCart);
+    
+    // const checkSum = () =>{
+    //     const timeSum = [];
+    //     addCart.map(data => timeSum.push(data.time));
+    // }
+    // console.log(timeSum);
+  
     useEffect(() => {
           fetch('books.json')
           .then(res => res.json())
           .then(data => setBook(data));
         },[])
         // console.log(book);
+    const handleAddToCart = (book) =>{
+    //    const newCart = [...addCart, book];
+       setCart(parseInt(book));
+        // console.log(book);
+        let sum = 0;
+        console.log(sum);
+          sum += parseInt(book);
+        
+        console.log(sum);
+    }
     return (
         <div className='layoout'>
             {/* main secton */}
             <div className='main-section'>
                 {
-                    book.map(book => <Book book={book}></Book>) 
+                    book.map(book => <Book 
+                        book={book}
+                        cart={handleAddToCart}
+                        
+                        ></Book>) 
                 }
-                {
-                    //  book.map(book => console.log(book))
-                }
-                
             </div>
-            {/* sidbar */}
+           
             <div className='sidebar'>
                 <h1>Salim Ahmed</h1>
                 <h3>Dhaka</h3>
@@ -33,7 +54,7 @@ const Layout = () => {
                 </div>
                 <div>
                     <h2>Book Details</h2>
-                    <p>Reading Time</p>
+                    <p>Reading Time: {} </p>
                     <p>Break Time</p>
                     <button>Activity Complete</button>
                 </div>
